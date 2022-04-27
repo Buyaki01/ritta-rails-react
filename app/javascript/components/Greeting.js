444346e2118edabcc1ffeebb383d507a8a2eb63b
Buyaki <rittahbuyaki@gmail.com>
2022-04-27 18:34:07 +0300

    class component to fetch message from the API

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,10 +1,27 @@
 import React from "react"
+import Axios from "axios"
 import PropTypes from "prop-types"
+
 class Greeting extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      greeting: ''
+    }
+  }
+
+  componentDidMount(){
+    const return_greeting = Axios.get('/v1/greetings')
+    .then(response => this.setState({ greeting: response.data.data.greeting.message })); 
+  }
+
   render () {
     return (
       <React.Fragment>
         Greeting: Hello
+        {
+          this.state.greeting
+        }
       </React.Fragment>
     );
   }
