3dce5475edc690694d659624388bd61ef92bf924
Buyaki <rittahbuyaki@gmail.com>
2022-04-27 20:54:17 +0300

    Add the functional component to fetch greeting message from the API

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,30 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Axios from "axios"
 import PropTypes from "prop-types"
 
-class Greeting extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      greeting: ''
-    }
-  }
+const Greeting = () => {
+  const [greeting, setGreeting] = useState([])
 
-  componentDidMount(){
-    const return_greeting = Axios.get('/v1/greetings')
-    .then(response => this.setState({ greeting: response.data.data.greeting.message })); 
+  const fetchGreeting = async () => {
+    const returnGreeting = await Axios.get('/v1/greetings');
+    // console.log(returnGreeting)
+    const data = returnGreeting.data.data.greeting.message
+    // console.log(data);
+    setGreeting(data);
   }
+  
+  useEffect( () => { fetchGreeting() }, [] );
 
-  render () {
-    return (
-      <React.Fragment>
-        Greeting: Hello
-        {
-          this.state.greeting
-        }
-      </React.Fragment>
-    );
-  }
+  return (
+    <div>
+      {greeting}
+    </div>
+  )
 }
-
 export default Greeting
