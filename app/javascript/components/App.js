f715b86ad7096cce3d298a0a9b386997e6bbb63f
Buyaki <rittahbuyaki@gmail.com>
2022-04-27 16:26:59 +0300

    Add BrowserRouter

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
 import PropTypes from "prop-types"
+import Greeting from "./Greeting";
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BrowserRouter>
+        <Greeting />
+      </BrowserRouter>
     );
   }
 }
