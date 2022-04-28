34092181c4a1306b5b111865a8411c1d8b4a7664
Buyaki <rittahbuyaki@gmail.com>
2022-04-28 07:41:27 +0300

    Add greetings reducer to the configureStore

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import greetingsReducer from './greetings/greetings';
 
 const reducer = combineReducers({
-   
+  greetingsReducer
 });
 
 const store = createStore(
