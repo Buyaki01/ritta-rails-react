0025a52e560e6ec0edf97cb4e8b467436906a8c9
Buyaki <rittahbuyaki@gmail.com>
2022-04-28 07:55:37 +0300

    Add Provider in App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,18 @@
 import React from "react"
+import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom"
 import PropTypes from "prop-types"
+import store from './redux/configureStore';
 import Greeting from "./Greeting";
+
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Greeting />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Greeting />
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
